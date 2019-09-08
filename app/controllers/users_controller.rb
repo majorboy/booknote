99c189e50cc2493688c40e0cecd1378379792c44
KengoNagata <majorboy47@gmail.com>
2019-09-09 07:04:21 +0900

    users_controller 各アクションの枠組みを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:email,:password, :password_confirmation)
   end
 end
