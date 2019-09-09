a89e1fdcddb3372a0087d325d4aebb8e7b2ffe06
KengoNagata <majorboy47@gmail.com>
2019-09-09 20:32:35 +0900

    updateアクション実装
    コールバックでedit /updateに共通な処理まとめ作業も併せて行った

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,38 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def create
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to books_path, notice: "「ユーザー登録を完了しました。"
+      redirect_to books_path, notice: "ユーザー登録が完了しました。"
     else
       render :new
     end
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to edit_user_path(@user), notice: "ユーザー情報を更新しました。"
+    else 
+      render :edit
+    end
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name,:email,:password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
