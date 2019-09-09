371e4a11be44f4d15220c8a5529e9c6abdbe33d0
KengoNagata <majorboy47@gmail.com>
2019-09-09 21:38:26 +0900

    書籍管理機能にアクセス制限を実装　非ログイン時はログイン画面にリダイレクトされるように設定した

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show,:edit,:update,:destroy]
+  before_action :login_required
 
   def index
     @books = Book.order('created_at DESC').page(params[:page]).per(5)
@@ -39,5 +40,9 @@ class BooksController < ApplicationController
 
   def set_book
     @book = Book.find(params[:id])
+  end
+
+  def login_required
+    redirect_to login_path unless current_user
   end
 end
