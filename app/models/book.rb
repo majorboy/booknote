e84d36e9fac224ae985169f5384e293b8c7ef4e1
KengoNagata <majorboy47@gmail.com>
2019-09-10 21:25:24 +0900

    Bookモデル、Noteモデル　アソシエーションを定義

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,5 @@ class Book < ApplicationRecord
   enum genre: [:money, :study_method, :programming]
   enum status: [:unfinished,:finished]
   belongs_to :user
+  has_one :note, dependent: :destroy
 end
