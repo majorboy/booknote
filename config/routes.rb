e12385be04eed66e0635ec5c5dd2e5dca332e6eb
KengoNagata <majorboy47@gmail.com>
2019-09-10 21:31:01 +0900

    Thoughtsコントローラを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'thoughts/new'
+
+  get 'thoughts/edit'
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
