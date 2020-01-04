e933d3b82580dc75b8f44e9b7893ee2b731709ce
KengoNagata <majorboy47@gmail.com>
2020-01-05 08:54:16 +0900

    new/createアクションを定義

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,8 +3,23 @@ class GenresController < ApplicationController
   end
 
   def new
+    @genre = Genre.new
   end
 
   def edit
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to genres_url, notice: "ジャンル「#{@genre.name}」を登録しました。"
+    else
+      render :new
+    end
+  end
+  
+  private
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
