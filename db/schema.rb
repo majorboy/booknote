2d9dfc94d89ab04e2359903c72e0f3173a888306
KengoNagata <majorboy47@gmail.com>
2020-01-05 08:54:59 +0900

    Booksテーブルに外部キーを追加
    目的
    本の登録時に「ジャンル」も併せて保存できるようにする為

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200104214708) do
+ActiveRecord::Schema.define(version: 20200104223150) do
 
   create_table "books", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title",                  null: false
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20200104214708) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.integer  "user_id"
+    t.integer  "genre_id"
+    t.index ["genre_id"], name: "index_books_on_genre_id", using: :btree
     t.index ["user_id"], name: "index_books_on_user_id", using: :btree
   end
 
@@ -51,5 +53,6 @@ ActiveRecord::Schema.define(version: 20200104214708) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "books", "genres"
   add_foreign_key "books", "users"
 end
