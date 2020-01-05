673a7ed9da2e240162a87757368ee58d4e11b775
KengoNagata <majorboy47@gmail.com>
2020-01-05 11:14:39 +0900

    edit/updateアクションを定義

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,4 +1,5 @@
 class GenresController < ApplicationController
+  before_action :set_genre, only: [:edit,:update,:destroy]
   def index 
     @genres = Genre.all
   end
@@ -18,9 +19,21 @@ class GenresController < ApplicationController
       render :new
     end
   end
+
+  def update
+    if @genre.update(genre_params)
+      redirect_to genres_url, notice: "ジャンル「#{@genre.name}」を更新しました。"
+    else
+      render :edit
+    end
+  end
   
   private
   def genre_params
     params.require(:genre).permit(:name)
+  end
+
+  def set_genre
+    @genre = Genre.find(params[:id])
   end
 end
